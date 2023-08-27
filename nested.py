6974620e733c571d47117eeb0d6302c229c93d80
aarjucode <119998969+aarjucode@users.noreply.github.com>
2023-08-27 08:32:51 +0545

    wauu

---
+++ nested.py
@@ -0,0 +1,29 @@
+""" year=int(input("enter the year to check for leap year: "))
+if(year % 4 ==0):
+    if (year % 12 ==0):
+        print(f"{year} is leap year")
+    else:
+        print("Nearly a leap year")
+else:
+    print("Not even close") """
+
+
+""" n=float(input("Enter a integer: "))
+if(n>0):
+    print(n**2)
+    flag=True
+elif(n<0):
+    print(n**3)
+    flag=True
+else:
+    pass """
+
+""" n=int(input("Enter a number: "))
+if(n%2==0):
+    print(f"{n} is even")
+else:
+    print (f"{n} is odd") """
+
+
+
+
